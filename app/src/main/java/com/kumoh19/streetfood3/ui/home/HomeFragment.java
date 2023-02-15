2bd58179528b9f79557615f02daf34f43a222673
psiyeon <psysy318@naver.com>
2023-02-15 15:38:58 +0900

    final upload

---
+++ app/src/main/java/com/kumoh19/streetfood3/ui/home/HomeFragment.java
@@ -0,0 +1,91 @@
+package com.kumoh19.streetfood3.ui.home;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.kumoh19.streetfood3.GpsTracker;
+import com.kumoh19.streetfood3.HomeActivity;
+import com.kumoh19.streetfood3.R;
+import com.kumoh19.streetfood3.databinding.FragmentHomeBinding;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class HomeFragment extends Fragment {
+
+    private GpsTracker gpsTracker;
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        MapView mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup) root.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        if (((HomeActivity)getActivity()).checkLocationServicesStatus()) {
+            ((HomeActivity)getActivity()).checkRunTimePermission();
+        } else {
+            ((HomeActivity)getActivity()).showDialogForLocationServiceSetting();
+        }
+
+        gpsTracker = new GpsTracker(getActivity());
+
+        double latitude = gpsTracker.getLatitude();
+        double longitude = gpsTracker.getLongitude();
+
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(latitude, longitude), true);
+        // 줌 레벨 변경
+        mapView.setZoomLevel(2, true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(latitude, longitude);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
